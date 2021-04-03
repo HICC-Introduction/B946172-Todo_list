ce9cf1d1e91dbf23b48a3117742795265ff0c6af
songhui714 <81204207+songhui714@users.noreply.github.com>
2021-04-04 03:25:54 +0900

    no message

--- todo.js
+++ todo.js
@@ -1,3 +1,13 @@
+"use strict"
+
+document.querySelector(".deco").addEventListener("click",function(e) { 
+    const tgt = e.target; 
+    if (tgt.classList.contains("fa-trash")) tgt.closest("li").remove(); 
+    })
+
+
+
+
 const timer = () => {
     const clock = document.querySelector('.clock');
     const time = new Date();
@@ -33,3 +43,74 @@ window.onload = start();
 
 
 
+
+
+const addButton = document.querySelector(".okay");
+var input = document.querySelector(".addToDoInfo");
+const container = document.querySelector(".container");
+
+class item{
+    constructor(itemName){
+        this.createDiv(itemName);
+    }
+
+    createDiv(itemName){
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input');
+        input.type = "text";
+
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML ="수정";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "삭제";
+        removeButton.classList.add('removeButton');
+
+
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+
+        editButton.addEventListener("click", () => this.edit(input) );
+        removeButton.addEventListener("click", () => this.remove(itemBox));
+
+
+    }
+
+    edit(input){
+        input.disabled = !input.disabled;
+    }
+
+    remove(item){
+        container.removeChild(item);
+    }
+}
+
+
+
+function check(){
+    if(input.value != ""){
+        new item(input.value);
+        input.value="";
+    }
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+    if(e.which == 13){
+        check();
+    }
+})
